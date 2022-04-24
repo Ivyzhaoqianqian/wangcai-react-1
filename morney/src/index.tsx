ed63b9d91cc9eee433e5bb405cc7c4e3bbf29afe
Ivyzhao <1490217104@qq.com>
2022-04-24 15:06:09 +0800

    eject

--- morney/src/index.tsx
+++ morney/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'index.scss';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById(elementId: 'root')
+  document.getElementById('root')
 );
 
 
