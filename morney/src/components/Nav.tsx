b50d9a86c6f21a55ced39e7baea3a9ec953375bb
Ivyzhao <1490217104@qq.com>
2022-04-24 16:08:39 +0800

    完成ICON

--- morney/src/components/Nav.tsx
+++ morney/src/components/Nav.tsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import React from 'react';
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
 
 
 
@@ -11,7 +16,15 @@ const NavWrapper = styled.nav`
     > li{
       width:33.333%;
       text-align:center;
-      padding:16px;
+      padding:4px 0;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      .icon{
+          width:24px;
+          height:24px;
+      }
     }
   }
 `;
@@ -21,12 +34,21 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag" />
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money" />
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#chart" />
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
