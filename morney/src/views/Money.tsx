834cb93e2c6364d8e01711d4d4feed2d7d4ae698
Ivyzhao <1490217104@qq.com>
2022-04-27 14:38:13 +0800

    完成备注功能

--- morney/src/views/Money.tsx
+++ morney/src/views/Money.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import React from "react";
 import { TagsSection } from "./Money/TagsSection";
-import { NotesSection } from "./Money/NotesSection";
+import { NoteSection } from "./Money/NoteSection";
 import { CategorySection } from "./Money/CategorySection";
 import { NumberPadSection } from "./Money/NumberPadSection";
 import styled from "styled-components";
@@ -16,13 +16,7 @@ function Money() {
     return (
         <MyLayout>
             <TagsSection />
-
-            <NotesSection>
-                <label>
-                    <span>备注</span>
-                    <input type="text" />
-                </label>
-            </NotesSection>
+            <NoteSection />
             <CategorySection>
                 <ul>
                     <li>支出</li>
