7fbe758479764b5d21955246bce7208bd54e88a3
Ivyzhao <1490217104@qq.com>
2022-04-27 14:23:29 +0800

    完成标签的选择

--- morney/src/views/Money/TagsSection.tsx
+++ morney/src/views/Money/TagsSection.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
-
-const TagsSection = styled.section`
+const Wrapper = styled.section`
     background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -10,7 +10,10 @@ const TagsSection = styled.section`
        background: #D9D9D9; border-radius: 18px;
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
-    }
+      &.selected{
+        background-color:#f60;
+      }
+      }
   }
   > button{
     background:none; border: none; padding: 2px 4px;
@@ -18,5 +21,37 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([])
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签名称为');
+    if (tagName !== null) {
+      setTags([...tags, tagName])
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag)
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag])
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={() => { onToggleTag(tag) }}
+            className={getClass(tag)}
+          >{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export { TagsSection };
