ab9fbce747aa8412827a39b1eaf1b074239b760e
Ivyzhao <1490217104@qq.com>
2022-04-27 15:00:03 +0800

    category切换功能完成

--- morney/src/views/Money/CategorySection.tsx
+++ morney/src/views/Money/CategorySection.tsx
@@ -1,6 +1,8 @@
+import { type } from "os";
+import { useState } from "react";
 import styled from "styled-components";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
     font-size: 24px;
   > ul{
     display:flex;
@@ -23,5 +25,25 @@ const CategorySection = styled.section`
     }
   }
 `;
+
+const CategorySection: React.FC = () => {
+  const categoryMap = { '-': '支出', '+': '收入' }
+  type Keys = keyof typeof categoryMap
+  const [categoryList] = useState<Keys[]>(['-', '+'])
+  const [category, setCategory] = useState('-') //+表示收入 -表示支出
+
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li className={category === c ? 'selected' : ''}
+            onClick={() => { setCategory(c) }}
+          >{categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper >
+  );
+};
 
 export { CategorySection };
