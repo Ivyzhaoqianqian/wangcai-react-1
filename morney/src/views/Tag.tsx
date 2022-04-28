391e4406fe7ac153971e772a051ad4b87bf4f9e9
Ivyzhao <1490217104@qq.com>
2022-04-28 16:44:11 +0800

    创建Tag.tsx

--- morney/src/views/Tag.tsx
+++ morney/src/views/Tag.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { useTags } from "../useTags";
 import { useParams } from "react-router-dom";
-import { type } from "os";
+import Layout from "../components/Layout";
+import Icon from "../components/Icon";
+import { Button } from "../components/Button";
+import styled from "styled-components";
+import { Input } from "../components/Input";
 
 type Params = {
     id: string
 }
+
+const Topbar = styled.header`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    line-height:20px;
+    padding:14px;
+    background-color:white;
+`
+
+
 
 const Tag: React.FC = () => {
     const { findTag } = useTags();
@@ -13,7 +28,23 @@ const Tag: React.FC = () => {
 
     const tag = findTag(parseInt(id));
     return (
-        <div>{tag.name}</div>
+        <Layout>
+            <Topbar>
+                <Icon name="left" />
+                <span>编辑标签</span>
+                <Icon />
+            </Topbar>
+
+            <div>
+                <Input>
+                    {/* <span>标签名</span> */}
+                    {/* <input type="text" placeholder="标签名" /> */}
+                </Input>
+            </div>
+            <div>
+                <Button>删除标签</Button>
+            </div>
+        </Layout>
     );
 };
 
