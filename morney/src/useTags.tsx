cd04168cb9ed2a3fd03e4821643464c3ea3b8d97
Ivyzhao <1490217104@qq.com>
2022-04-28 14:30:37 +0800

    重构 将TagString重构为TagId

--- morney/src/useTags.tsx
+++ morney/src/useTags.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useTags = () => {
-    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(['衣', '食', '住', '行']);
     return { tags, setTags };
 };
 
